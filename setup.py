09ebf2840dff924a6457e3faaff9c9b32a4f1a9f
Armin Ronacher <armin.ronacher@active-4.com>
2012-10-06 12:15:29 +0200

    This is 0.13

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='Flask-OAuth',
-    version='0.12',
+    version='0.13',
     url='http://github.com/mitsuhiko/flask-oauth',
     license='BSD',
     author='Armin Ronacher',
