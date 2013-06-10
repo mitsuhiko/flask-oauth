66e1b1395d422c767ec649ad611503de68675a23
Eric van Zanten <eric@bahai.us>
2013-06-10 09:59:10 -0500

    Attempting to add ability to include other SSL certs

--- flask_oauth.py
+++ flask_oauth.py
@@ -178,7 +178,8 @@ class OAuthRemoteApp(object):
                  consumer_key, consumer_secret,
                  request_token_params=None,
                  access_token_params=None,
-                 access_token_method='GET'):
+                 access_token_method='GET',
+                 additional_certs=None):
         self.oauth = oauth
         #: the `base_url` all URLs are joined with.
         self.base_url = base_url
@@ -195,6 +196,8 @@ class OAuthRemoteApp(object):
         self._consumer = oauth2.Consumer(self.consumer_key,
                                          self.consumer_secret)
         self._client = OAuthClient(self._consumer)
+        if additional_certs:
+            self._client.ca_certs = additional_certs
 
     def status_okay(self, resp):
         """Given request data, checks if the status is okay."""
