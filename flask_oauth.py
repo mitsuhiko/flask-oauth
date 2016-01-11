43fdeaedd312bd70c7fa49c6a09cd0faf23e4a86
Hammy Goonan <hammy@spiresoftware.com.au>
2016-01-11 10:07:45 -0600

    changing urljoin import for Python3 compatability.

--- flask_oauth.py
+++ flask_oauth.py
@@ -10,7 +10,7 @@
 """
 import httplib2
 from functools import wraps
-from urlparse import urljoin
+from urllib.parse import urljoin
 from flask import request, session, json, redirect, Response
 from werkzeug import url_decode, url_encode, url_quote, \
      parse_options_header, Headers
